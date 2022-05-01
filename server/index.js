5a391c205db104d118883687f9fc95701f457bab
Mohamed Aden <mo-aden@Mac-Pro.hsd1.co.comcast.net>
2022-04-30 22:00:07 -0600

    completed the question post

--- server/index.js
+++ server/index.js
@@ -6,20 +6,22 @@ import cors from "cors";
 import postRoutes from "./routes/posts.js";
 
 const app = express();
+app.use(cors)
+
 
 app.use("/posts", postRoutes);
 
 app.use(bodyParser.json({limit: "30mb", extended: true}))
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
-app.use(cors)
 
 
 
 //Connection to mongoDB url
-const CONNECTION_URL = "mongodb+srv://mo-app:XcURX70SXcJBmUOL@cluster0.5e5ea.mongodb.net/mo-appDB?retryWrites=true&w=majority";
+const CONNECTION_URL = "mongodb+srv://mo-app:Hoyomacan01@cluster0.5e5ea.mongodb.net/Mo-appDB"
 
 const PORT = process.env.PORT || 8080;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
-.then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-.catch((error) => console.log(error.message));
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
