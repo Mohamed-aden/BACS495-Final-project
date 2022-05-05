34b30bdbba342a3a8aa87700d35d18648e7fb3a3
mohamed-aden <aden6864@bears.unco.edu>
2022-05-04 22:19:46 -0600

    modified mongoDB connection

--- server/index.js
+++ server/index.js
@@ -21,7 +21,10 @@ const CONNECTION_URL = "mongodb+srv://mo-app:<QSFsS1WfwFhk8r7v>@cluster0.5e5ea.m
 
 const PORT = process.env.PORT || 8080;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-    .catch((error) => console.log(error.message));
+mongoose.connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => console.log("MongoDB Connected !!"))
+    .catch((err) => console.log(err.message));
 
